4600392c71672dabe670da148f99344819f99562
kennethteh90 <kennethteh90@gmail.com>
2018-02-03 11:57:26 +0800

    added request specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -35,6 +35,9 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
+
+  # add JSON parse module
+  config.include RequestSpecHelper, type: :request
 
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
